a7d28d1362ed9615f5f93078efc50f5d1aa68b3b
Ahmetereni <94352384+Ahmetereni@users.noreply.github.com>
2023-03-25 15:36:23 +0300

    Add files via upload

---
+++ rastgele_set/rasgeleSet.cpp
@@ -0,0 +1,56 @@
+﻿// rasgeleSet.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+#include <unordered_set>
+
+#include <stdlib.h>
+#include <time.h>
+#define MAX_DEGER 9
+#define MIN_DEGER 0
+using namespace std;
+
+void print_random_set() {
+	srand(time(NULL));
+	int random_number = rand() % (MAX_DEGER + 1) + MIN_DEGER;
+	cout << random_number;
+
+	unordered_set<int> set_of_int = {}; // defining a set with values
+
+	std::cout << '\n';
+
+	while (set_of_int.size() < 10) {
+		set_of_int.insert(rand() % (MAX_DEGER + 1) + MIN_DEGER);
+
+	}
+	for (auto& str : set_of_int) {
+		std::cout << str << ' ';
+	}
+}
+
+void main_loop() {
+	bool again = true;
+
+	while (again) {
+		print_random_set();
+		string answer = "n";
+		cout << "DO again ? (n for no)\n\n";
+		cin >> answer;
+		if (answer == "n")
+		{
+			break;
+		}
+		else
+		{
+			print_random_set();
+		}
+
+	}
+}
+
+	int main()
+	{
+
+		main_loop();
+		return 0;
+	}
